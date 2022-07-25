304badcff6e4389ed06e0803304f9b0d2c9af77c
Miguel Dantas <migueldantas@rits.com.br>
2022-07-25 20:36:30 -0300

    :tada:First commit

---
+++ src/test/java/com/miguel/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
